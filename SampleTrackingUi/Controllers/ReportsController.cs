d8e18d04ec456aa194def734f42e21612da2392d
dannenbring.llc <dannenbring.llc@gmail.com>
2020-11-15 12:10:46 -0500

    Added Clear Mini Report.

--- SampleTrackingUi/Controllers/ReportsController.cs
+++ SampleTrackingUi/Controllers/ReportsController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SampleTrackingUi.Services;
+using SampleTrackingUi.ViewModels.Reports;
+using System.Threading.Tasks;
 
 namespace SampleTrackingUi.Controllers
 {
     public class ReportsController : Controller
     {
+        private readonly IIGTSamplesApi _igtSamplesApi;
+
+        public ReportsController(IIGTSamplesApi igtSamplesApi)
+        {
+            _igtSamplesApi = igtSamplesApi;
+        }
+
         public IActionResult TrayMap()
         {
             return View();
+        }
+
+        [HttpGet("ClearMini")]
+        public async Task<IActionResult> ClearMini(ClearMiniViewModel viewModel)
+        {
+            if (viewModel.LogNumber != null)
+            {
+                var sample = await _igtSamplesApi.GetSampleAsync(viewModel.LogNumber);
+                if (sample == null)
+                {
+                    viewModel.ShowReportButton = false;
+                    return View(viewModel);
+                }
+                viewModel.LogNumber = sample.KbNumber;
+                viewModel.PatId = sample.PatientId;
+                viewModel.PatientName = sample.PatientName;
+                viewModel.ShowReportButton = true;
+            }
+
+            return View(viewModel);
         }
     }
 }
---
+++ SampleTrackingUi/ViewModels/Reports/ClearMiniViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleTrackingUi.ViewModels.Reports
+{
+    public class ClearMiniViewModel
+    {
+        public string LogNumber { get; set; }
+        public string PatId { get; set; }
+        public string PatientName { get; set; }
+        public bool ShowReportButton { get; set; }
+    }
+}
